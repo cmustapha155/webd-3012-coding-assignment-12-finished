8e497859608fd2f2d51782242252c5ff639f6cfd
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-14 02:08:36 -0500

    Add index.ts files to components/ and src/

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Button';
+export * from './Label';
+
+// Add more exports for other components as needed
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// Export all components from 'components' folder
+
+export * from './components';
