434b674281b968ec6ab57f49467b23d7c996ad8e
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-15 14:50:54 -0500

    Add Button.stories.tsx

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+// Meta object containing component metadata
+const meta: Meta<typeof Button> = {
+  title: "Button",
+  component: Button,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+// Stories
+export const Primary: Story = {
+  args: {
+    text: "Click Me",
+    className: "button",
+    id: "",
+    hoverBackgroundColor: "", // Default (#3284ad) set in Button.tsx
+    type: "button",
+    onClick: () => console.log("Click"),
+    disabled: false,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    text: "Click Me",
+    className: "button",
+    id: "",
+    hoverBackgroundColor: "", // Default (#3284ad) set in Button.tsx
+    type: "button",
+    onClick: () => {}, // No behaviour
+    disabled: true,
+  },
+};
+
+export const PurpleGrey: Story = {
+  args: {
+    text: "Click Me",
+    className: "button",
+    id: "",
+    hoverBackgroundColor: "#4c5172",
+    type: "button",
+    disabled: false,
+  },
+};
