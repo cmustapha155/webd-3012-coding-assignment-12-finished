cae72be755758f0c6a2980992f99d6ebaf72a41c
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-15 15:08:05 -0500

    Add Label.stories.tsx

---
+++ src/components/Label/Label.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Label from "./Label";
+
+// Meta object containing component metadata
+const meta: Meta<typeof Label> = {
+  title: "Label",
+  component: Label,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Label>;
+
+// Stories
+export const Primary: Story = {
+  args: {
+    text: "Label Text",
+    className: "label",
+    id: "",
+    for: "",
+    disabled: false,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    text: "Label Text",
+    className: "label",
+    id: "",
+    for: "",
+    disabled: true,
+  },
+};
+
+export const PurpleGrey: Story = {
+  args: {
+    text: "Label Text",
+    className: "label",
+    id: "",
+    for: "",
+    disabled: false,
+    textColor: "#4c5172",
+  },
+};
